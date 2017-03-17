9f6f0b1c3c3b7de4862293e51f0ae9d6de143771
Michal Jez <Michal.G.Jez@gmail.com>
2017-03-17 00:29:09 -0400

    made header mobile friendly

--- header.js
+++ header.js
@@ -1,8 +1,16 @@
 
 
 $(window).ready(function () {
-    console.log("HEY");
+
+    //If one of the header buttons are clicked it scrolls to the desired spot
     $("#header li.about").click(function () {
         $("#about").ScrollTo();
+    });
+
+    $(window).resize(function () {
+        if ($(this).width() < 620)
+        {
+            $("#header ul").hide();
+        }
     });
 });
